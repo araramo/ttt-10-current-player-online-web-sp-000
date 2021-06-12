414a0d8344a9c02218c1425119a6b82098918b12
Araceli Ramos <celi2455@gmail.com>
2021-06-12 22:22:26 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,10 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+
+  def current_player(board)
+    turn_count(board) 
